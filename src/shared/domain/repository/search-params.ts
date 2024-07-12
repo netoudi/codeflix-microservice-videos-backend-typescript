59befbb944d6e2622b374f3b5258691976f2cb94
netoudi <netoudi@rocketmail.com>
2024-07-12 15:45:55 -0300

    testando-abstracao-de-entrada-e-saida-de-busca-em-repositorios

--- src/shared/domain/repository/search-params.ts
+++ src/shared/domain/repository/search-params.ts
@@ -19,11 +19,11 @@ export class SearchParams<Filter = string> extends ValueObject {
 
   constructor(props: SearchParamsConstructor<Filter> = {}) {
     super();
-    this._page = props.page;
-    this._perPage = props.perPage;
-    this._sort = props.sort;
-    this._sortDir = props.sortDir;
-    this._filter = props.filter;
+    this.page = props.page;
+    this.perPage = props.perPage;
+    this.sort = props.sort;
+    this.sortDir = props.sortDir;
+    this.filter = props.filter;
   }
 
   get page(): number {
