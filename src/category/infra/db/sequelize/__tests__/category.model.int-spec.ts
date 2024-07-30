47b0f7631abbc93a9fbdcf5d5c00ee91202edc2c
netoudi <netoudi@rocketmail.com>
2024-07-30 10:10:10 -0300

    criando-testes-para-o-model-de-categoria

--- src/category/infra/db/sequelize/__tests__/category.model.int-spec.ts
+++ src/category/infra/db/sequelize/__tests__/category.model.int-spec.ts
@@ -1,24 +1,77 @@
-import { Sequelize } from 'sequelize-typescript';
-import { Category } from '@/category/domain/category.entity';
+import { DataType, Sequelize } from 'sequelize-typescript';
 import { CategoryModel } from '@/category/infra/db/sequelize/category.model';
 
-describe('CategoryModel Unit Test', () => {
-  test('should create a category', async () => {
-    const sequelize = new Sequelize({
+describe('CategoryModel Integration Tests', () => {
+  let sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
       dialect: 'sqlite',
       storage: ':memory:',
+      logging: false,
       models: [CategoryModel],
     });
     await sequelize.sync({ force: true });
+  });
 
-    const category = Category.fake().aCategory().build();
+  test('mapping props', () => {
+    const attributesMap = CategoryModel.getAttributes();
+    const attributes = Object.keys(CategoryModel.getAttributes());
+    expect(attributes).toStrictEqual(['id', 'name', 'description', 'is_active', 'created_at']);
 
-    await CategoryModel.create({
-      id: category.id.value,
-      name: category.name,
-      description: category.description,
-      is_active: category.isActive,
-      created_at: category.createdAt,
+    const categoryIdAttr = attributesMap.id;
+    expect(categoryIdAttr).toMatchObject({
+      field: 'id',
+      fieldName: 'id',
+      primaryKey: true,
+      type: DataType.UUID(),
     });
+
+    const nameAttr = attributesMap.name;
+    expect(nameAttr).toMatchObject({
+      field: 'name',
+      fieldName: 'name',
+      allowNull: false,
+      type: DataType.STRING(255),
+    });
+
+    const descriptionAttr = attributesMap.description;
+    expect(descriptionAttr).toMatchObject({
+      field: 'description',
+      fieldName: 'description',
+      allowNull: true,
+      type: DataType.TEXT(),
+    });
+
+    const isActiveAttr = attributesMap.is_active;
+    expect(isActiveAttr).toMatchObject({
+      field: 'is_active',
+      fieldName: 'is_active',
+      allowNull: false,
+      type: DataType.BOOLEAN(),
+    });
+
+    const createdAtAttr = attributesMap.created_at;
+    expect(createdAtAttr).toMatchObject({
+      field: 'created_at',
+      fieldName: 'created_at',
+      allowNull: false,
+      type: DataType.DATE(3),
+    });
+  });
+
+  test('create', async () => {
+    // arrange
+    const arrange = {
+      id: 'c3c3c3c3-c3c3-c3c3-c3c3-c3c3c3c3c3c3',
+      name: 'Movie',
+      description: 'Movie description',
+      is_active: true,
+      created_at: new Date(),
+    };
+    // act
+    const category = await CategoryModel.create(arrange);
+    // assert
+    expect(category.toJSON()).toStrictEqual(arrange);
   });
 });
