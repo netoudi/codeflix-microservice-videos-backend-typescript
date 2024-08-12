32c2a8e0d2e448b6a94e694406ef18e84830eaeb
netoudi <netoudi@rocketmail.com>
2024-08-12 08:44:44 -0300

    criando-servico-de-configuracao-para-os-testes

---
+++ src/shared/infra/config.ts
@@ -0,0 +1,20 @@
+import { join } from 'node:path';
+import { config as readEnv } from 'dotenv';
+
+export class Config {
+  static env: any = null;
+
+  static db() {
+    Config.readEnv();
+    return {
+      dialect: Config.env.DB_CONNECTION,
+      host: Config.env.DB_HOST,
+      logging: Config.env.DB_LOGGING === 'true',
+    };
+  }
+
+  static readEnv() {
+    if (Config.env) return;
+    Config.env = readEnv({ path: join(__dirname, `../../../envs/.env.${process.env.NODE_ENV}`) }).parsed;
+  }
+}
