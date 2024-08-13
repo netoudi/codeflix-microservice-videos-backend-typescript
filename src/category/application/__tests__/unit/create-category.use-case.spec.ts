65e3200ccb71048009658ca2441ec3cbbf9a8b9e
netoudi <netoudi@rocketmail.com>
2024-08-13 08:44:44 -0300

    testes-do-caso-de-uso-de-criacao-de-categoria

---
+++ src/category/application/__tests__/unit/create-category.use-case.spec.ts
@@ -0,0 +1,34 @@
+import { CreateCategoryUseCase } from '@/category/application/create-category.use-case';
+import { CategoryInMemoryRepository } from '@/category/infra/db/in-memory/category-in-memory.repository';
+
+describe('CreateCategoryUseCase Unit Tests', () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  it('should create a new category', async () => {
+    const spyInsert = jest.spyOn(repository, 'insert');
+    let output = await useCase.execute({ name: 'test' });
+    expect(spyInsert).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual({
+      id: repository.items[0].id.value,
+      name: 'test',
+      description: null,
+      isActive: true,
+      createdAt: repository.items[0].createdAt,
+    });
+    output = await useCase.execute({ name: 'test', description: 'some description', isActive: false });
+    expect(spyInsert).toHaveBeenCalledTimes(2);
+    expect(output).toStrictEqual({
+      id: repository.items[1].id.value,
+      name: 'test',
+      description: 'some description',
+      isActive: false,
+      createdAt: repository.items[1].createdAt,
+    });
+  });
+});
