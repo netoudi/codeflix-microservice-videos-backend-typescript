c0ea7d7c4f6c6158a365f28ed03484ab7d442a2e
netoudi <netoudi@rocketmail.com>
2024-08-14 09:57:57 -0300

    caso-de-uso-de-listagem-de-categorias

--- src/category/application/use-cases/common/category-output.mapper.ts
+++ src/category/application/use-cases/common/category-output.mapper.ts
@@ -1,4 +1,9 @@
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/category/application/use-cases/common/pagination-output.mapper';
 import { Category } from '@/category/domain/category.entity';
+import { CategorySearchResult } from '@/category/domain/category.repository';
 
 export type CategoryOutput = {
   id: string;
@@ -17,5 +22,12 @@ export class CategoryOutputMapper {
       isActive: category.isActive,
       createdAt: category.createdAt,
     };
+  }
+
+  static toPagination(result: CategorySearchResult): PaginationOutput<CategoryOutput> {
+    return PaginationOutputMapper.toOutput(
+      result.items.map((category) => CategoryOutputMapper.toOutput(category)),
+      result,
+    );
   }
 }
