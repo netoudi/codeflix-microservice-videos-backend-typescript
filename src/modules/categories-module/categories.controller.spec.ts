b2b8f7f359b2396b0f9a7e407a109c207db8c0eb
netoudi <netoudi@rocketmail.com>
2024-08-26 11:11:11 -0300

    primeiro-teste-do-controller-de-categoria

--- src/modules/categories-module/categories.controller.spec.ts
+++ src/modules/categories-module/categories.controller.spec.ts
@@ -1,4 +1,7 @@
+import { getModelToken, SequelizeModule } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CategorySequelizeRepository } from '@/core/category/infra/db/sequelize/category-sequelize.repository';
+import { CategoryModel } from '@/core/category/infra/db/sequelize/category.model';
 import { CategoriesController } from '@/modules/categories-module/categories.controller';
 
 describe('CategoriesController', () => {
@@ -6,7 +9,25 @@ describe('CategoriesController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRoot({
+          dialect: 'sqlite',
+          host: ':memory:',
+          logging: false,
+          models: [CategoryModel],
+        }),
+        SequelizeModule.forFeature([CategoryModel]),
+      ],
       controllers: [CategoriesController],
+      providers: [
+        {
+          provide: CategorySequelizeRepository,
+          useFactory: (categoryModel: typeof CategoryModel) => {
+            return new CategorySequelizeRepository(categoryModel);
+          },
+          inject: [getModelToken(CategoryModel)],
+        },
+      ],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
