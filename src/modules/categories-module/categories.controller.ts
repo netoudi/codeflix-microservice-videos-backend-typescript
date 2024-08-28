1d31582c59ed3ab84994c4fea655f85e80d58ba9
netoudi <netoudi@rocketmail.com>
2024-08-28 09:51:00 -0300

    usando-providers-no-controller-de-categoria

--- src/modules/categories-module/categories.controller.ts
+++ src/modules/categories-module/categories.controller.ts
@@ -1,13 +1,28 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { CategorySequelizeRepository } from '@/core/category/infra/db/sequelize/category-sequelize.repository';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post } from '@nestjs/common';
+import { CreateCategoryUseCase } from '@/core/category/application/use-cases/create-category/create-category.use-case';
+import { DeleteCategoryUseCase } from '@/core/category/application/use-cases/delete-category/delete-category.use-case';
+import { GetCategoryUseCase } from '@/core/category/application/use-cases/get-category/get-category.use-case';
+import { ListCategoriesUseCase } from '@/core/category/application/use-cases/list-category/list-categories.use-case';
+import { UpdateCategoryUseCase } from '@/core/category/application/use-cases/update-category/update-category.use-case';
 import { CreateCategoryDto } from '@/modules/categories-module/dto/create-category.dto';
 import { UpdateCategoryDto } from '@/modules/categories-module/dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private categoryRepository: CategorySequelizeRepository) {
-    console.log(this.categoryRepository);
-  }
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase;
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteUseCase: DeleteCategoryUseCase;
+
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase;
+
+  @Inject(ListCategoriesUseCase)
+  private: ListCategoriesUseCase;
+
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase;
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
