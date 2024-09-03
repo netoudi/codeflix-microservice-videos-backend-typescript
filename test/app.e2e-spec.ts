e812c2674ce4b576a0913b4ec9a2b56c0ac084a2
netoudi <netoudi@rocketmail.com>
2024-09-03 09:51:51 -0300

    iniciando-configuracao-para-testes-end-to-end

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest';
+import { AppModule } from '@/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,7 +15,12 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    await app?.close();
+  });
+
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    expect(process.env.NODE_ENV).toBe('e2e');
+    return request(app.getHttpServer()).get('/categories').expect(200);
   });
 });
