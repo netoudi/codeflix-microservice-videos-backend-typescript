6d0ec360f8372d740447ad09e99d2e946e983f85
netoudi <netoudi@rocketmail.com>
2024-09-03 11:37:00 -0300

    configuracao-para-recriar-tabelas-a-cada-teste-end-to-end

--- src/modules/shared-module/testing/helpers.ts
+++ src/modules/shared-module/testing/helpers.ts
@@ -1,5 +1,7 @@
 import { INestApplication } from '@nestjs/common';
+import { getConnectionToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Sequelize } from 'sequelize-typescript';
 import { AppModule } from '@/app.module';
 import { applyGlobalConfig } from '@/modules/global-config';
 
@@ -10,6 +12,8 @@ export function startApp() {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    const sequelize = moduleFixture.get<Sequelize>(getConnectionToken());
+    await sequelize.sync({ force: true });
     _app = moduleFixture.createNestApplication();
     applyGlobalConfig(_app);
     await _app.init();
