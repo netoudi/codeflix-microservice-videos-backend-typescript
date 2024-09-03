1e4147f04d875069b6e91a5597e1a9acf0c9f3c5
netoudi <netoudi@rocketmail.com>
2024-09-03 10:39:39 -0300

    criando-helper-de-teste-para-iniciar-um-app-nest

--- test/categories/create-category.e2e-spec.ts
+++ test/categories/create-category.e2e-spec.ts
@@ -1,35 +1,21 @@
-import { INestApplication } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
-import { AppModule } from '@/app.module';
 import { ICategoryRepository } from '@/core/category/domain/category.repository';
 import { CATEGORY_PROVIDERS } from '@/modules/categories-module/categories.providers';
 import { CreateCategoryFixture } from '@/modules/categories-module/testing/category-fixture';
-import { applyGlobalConfig } from '@/modules/global-config';
+import { startApp } from '@/modules/shared-module/testing/helpers';
 
 describe('CategoriesController (e2e)', () => {
-  let app: INestApplication;
+  const appHelper = startApp();
   let repository: ICategoryRepository;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    applyGlobalConfig(app);
-    await app.init();
-    repository = app.get<ICategoryRepository>(CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide);
-  });
-
-  afterEach(async () => {
-    await app?.close();
+    repository = appHelper.app.get<ICategoryRepository>(CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide);
   });
 
   describe('/categories (POST)', () => {
     const arrange = CreateCategoryFixture.arrangeForCreate();
     test.each(arrange)('when body is $send_data', async ({ send_data, expected }) => {
-      const response = await request(app.getHttpServer()).post('/categories').send(send_data).expect(201);
+      const response = await request(appHelper.app.getHttpServer()).post('/categories').send(send_data).expect(201);
       const keysInResponse = CreateCategoryFixture.keysInResponse;
       console.log(response.body);
       expect(Object.keys(response.body)).toStrictEqual(['data']);
