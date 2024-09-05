47ff4b7b9f7830e49d41b1ef1d8c166dd9c1500b
netoudi <netoudi@rocketmail.com>
2024-09-05 09:21:51 -0300

    criando-instancia-do-umzug

---
+++ src/core/shared/infra/db/sequelize/migrator.ts
@@ -0,0 +1,21 @@
+import { join } from 'node:path';
+import { Sequelize } from 'sequelize';
+import { SequelizeStorage, Umzug, UmzugOptions } from 'umzug';
+
+export function migrator(sequelize: Sequelize, options?: Partial<UmzugOptions>) {
+  return new Umzug({
+    migrations: {
+      glob: [
+        '*/infra/db/sequelize/migrations/*.{js,ts}',
+        {
+          cwd: join(__dirname, '..', '..', '..', '..'),
+          ignore: ['**/*.ds.ts', '**/index.ts', '**/index.js'],
+        },
+      ],
+    },
+    context: sequelize,
+    storage: new SequelizeStorage({ sequelize }),
+    logger: console,
+    ...(options || {}),
+  });
+}
