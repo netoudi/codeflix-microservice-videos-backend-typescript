deb497dc29daff5681545e86cef3f4695c717b5b
netoudi <netoudi@rocketmail.com>
2024-10-03 09:10:00 -0300

    sobre-saida-dos-casos-de-uso-de-genero

--- src/core/genre/application/use-cases/common/genre-output.mapper.spec.ts
+++ src/core/genre/application/use-cases/common/genre-output.mapper.spec.ts
@@ -1,48 +1,37 @@
+import { Category } from '@/core/category/domain/category.entity';
 import { GenreOutputMapper } from '@/core/genre/application/use-cases/common/genre-output.mapper';
 import { Genre } from '@/core/genre/domain/genre.aggregate';
-import { GenreSearchResult } from '@/core/genre/domain/genre.repository';
 
 describe('GenreOutputMapper Unit Tests', () => {
   it('should convert a genre in output', () => {
-    const genre = Genre.fake().aGenre().build();
-    const output = GenreOutputMapper.toOutput(genre);
+    const categories = Category.fake().theCategories(2).build();
+    const created_at = new Date();
+    const entity = Genre.fake()
+      .aGenre()
+      .withName('test')
+      .addCategoryId(categories[0].id)
+      .addCategoryId(categories[1].id)
+      .withCreatedAt(created_at)
+      .build();
+    const output = GenreOutputMapper.toOutput(entity, categories);
     expect(output).toStrictEqual({
-      id: genre.id.value,
-      name: genre.name,
-      description: genre.description,
-      is_active: genre.is_active,
-      created_at: genre.created_at,
-    });
-  });
-
-  it('should convert search result to pagination', () => {
-    let result = new GenreSearchResult({
-      items: [],
-      total: 0,
-      current_page: 1,
-      per_page: 1,
-    });
-    let output = GenreOutputMapper.toPagination(result);
-    expect(output).toStrictEqual({
-      items: [],
-      total: result.total,
-      current_page: result.current_page,
-      per_page: result.per_page,
-      last_page: result.last_page,
-    });
-    result = new GenreSearchResult({
-      items: Genre.fake().theGenres(3).build(),
-      total: 3,
-      current_page: 1,
-      per_page: 15,
-    });
-    output = GenreOutputMapper.toPagination(result);
-    expect(output).toStrictEqual({
-      items: result.items.map(GenreOutputMapper.toOutput),
-      total: result.total,
-      current_page: result.current_page,
-      per_page: result.per_page,
-      last_page: result.last_page,
+      id: entity.id.value,
+      name: 'test',
+      categories: [
+        {
+          id: categories[0].id.value,
+          name: categories[0].name,
+          created_at: categories[0].created_at,
+        },
+        {
+          id: categories[1].id.value,
+          name: categories[1].name,
+          created_at: categories[1].created_at,
+        },
+      ],
+      categories_id: [categories[0].id.value, categories[1].id.value],
+      is_active: entity.is_active,
+      created_at,
     });
   });
 });
--- src/core/genre/application/use-cases/common/genre-output.mapper.ts
+++ src/core/genre/application/use-cases/common/genre-output.mapper.ts
@@ -1,27 +1,34 @@
+import { Category } from '@/core/category/domain/category.entity';
 import { Genre } from '@/core/genre/domain/genre.aggregate';
-import { GenreSearchResult } from '@/core/genre/domain/genre.repository';
-import { PaginationOutput, PaginationOutputMapper } from '@/core/shared/application/pagination-output.mapper';
+
+export type GenreCategoryOutput = {
+  id: string;
+  name: string;
+  created_at: Date;
+};
 
 export type GenreOutput = {
   id: string;
   name: string;
-  description: string | null;
+  categories: GenreCategoryOutput[];
+  categories_id: string[];
   is_active: boolean;
   created_at: Date;
 };
 
 export class GenreOutputMapper {
-  static toOutput(genre: Genre): GenreOutput {
+  static toOutput(genre: Genre, categories: Category[]): GenreOutput {
     return {
       id: genre.id.value,
       name: genre.name,
-      description: genre.description,
+      categories: categories.map((c) => ({
+        id: c.id.value,
+        name: c.name,
+        created_at: c.created_at,
+      })),
+      categories_id: categories.map((c) => c.id.value),
       is_active: genre.is_active,
       created_at: genre.created_at,
     };
-  }
-
-  static toPagination(result: GenreSearchResult): PaginationOutput<GenreOutput> {
-    return PaginationOutputMapper.toOutput(result.items.map(GenreOutputMapper.toOutput), result);
   }
 }
