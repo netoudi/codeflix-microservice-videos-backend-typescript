48f067e946d91583abc98440f8d7318fa991258d
netoudi <netoudi@rocketmail.com>
2024-10-04 09:01:31 -0300

    criando-caso-de-uso-de-pegar-um-genero

--- src/core/genre/application/use-cases/get-genre/__tests__/get-genre.use-case.spec.ts
+++ src/core/genre/application/use-cases/get-genre/__tests__/get-genre.use-case.spec.ts
@@ -1,36 +1,54 @@
+import { Category } from '@/core/category/domain/category.entity';
+import { CategoryInMemoryRepository } from '@/core/category/infra/db/in-memory/category-in-memory.repository';
 import { GetGenreUseCase } from '@/core/genre/application/use-cases/get-genre/get-genre.use-case';
-import { Genre } from '@/core/genre/domain/genre.aggregate';
+import { GenreId, Genre } from '@/core/genre/domain/genre.aggregate';
 import { GenreInMemoryRepository } from '@/core/genre/infra/db/in-memory/genre-in-memory.repository';
 import { NotFoundError } from '@/core/shared/domain/errors/not-found';
-import { InvalidUuidError, Uuid } from '@/core/shared/domain/value-objects/uuid.vo';
 
 describe('GetGenreUseCase Unit Tests', () => {
+  let genreRepository: GenreInMemoryRepository;
+  let categoryRepository: CategoryInMemoryRepository;
   let useCase: GetGenreUseCase;
-  let repository: GenreInMemoryRepository;
 
   beforeEach(() => {
-    repository = new GenreInMemoryRepository();
-    useCase = new GetGenreUseCase(repository);
+    genreRepository = new GenreInMemoryRepository();
+    categoryRepository = new CategoryInMemoryRepository();
+    useCase = new GetGenreUseCase(genreRepository, categoryRepository);
   });
 
   it('should throw error when entity not found', async () => {
-    await expect(useCase.execute({ id: 'fake-id' })).rejects.toThrow(new InvalidUuidError());
-    const uuid = new Uuid();
-    await expect(useCase.execute({ id: uuid.value })).rejects.toThrow(new NotFoundError(uuid.value, Genre));
+    const genreId = new GenreId();
+    await expect(() => useCase.execute({ id: genreId.value })).rejects.toThrow(new NotFoundError(genreId.value, Genre));
   });
 
   it('should return a genre', async () => {
-    const items = [Genre.fake().aGenre().build()];
-    repository.items = items;
-    const spyFindById = jest.spyOn(repository, 'findById');
-    const output = await useCase.execute({ id: items[0].id.value });
-    expect(spyFindById).toHaveBeenCalledTimes(1);
+    const categories = Category.fake().theCategories(3).build();
+    await categoryRepository.bulkInsert(categories);
+    const genre = Genre.fake().aGenre().addCategoryId(categories[0].id).addCategoryId(categories[2].id).build();
+    genreRepository.items = [genre];
+    const spyGenreFindById = jest.spyOn(genreRepository, 'findById');
+    const spyCategoryFindByIds = jest.spyOn(categoryRepository, 'findByIds');
+    const output = await useCase.execute({ id: genre.id.value });
+    expect(spyGenreFindById).toHaveBeenCalledTimes(1);
+    expect(spyCategoryFindByIds).toHaveBeenCalledTimes(1);
     expect(output).toStrictEqual({
-      id: items[0].id.value,
-      name: items[0].name,
-      description: items[0].description,
-      is_active: items[0].is_active,
-      created_at: items[0].created_at,
+      id: genre.id.value,
+      name: genre.name,
+      categories: [
+        {
+          id: categories[0].id.value,
+          name: categories[0].name,
+          created_at: categories[0].created_at,
+        },
+        {
+          id: categories[2].id.value,
+          name: categories[2].name,
+          created_at: categories[2].created_at,
+        },
+      ],
+      categories_id: [...genre.categories_id.keys()],
+      is_active: true,
+      created_at: genre.created_at,
     });
   });
 });
--- src/core/genre/application/use-cases/get-genre/get-genre.use-case.ts
+++ src/core/genre/application/use-cases/get-genre/get-genre.use-case.ts
@@ -1,3 +1,4 @@
+import { ICategoryRepository } from '@/core/category/domain/category.repository';
 import { GenreOutput, GenreOutputMapper } from '@/core/genre/application/use-cases/common/genre-output.mapper';
 import { Genre, GenreId } from '@/core/genre/domain/genre.aggregate';
 import { IGenreRepository } from '@/core/genre/domain/genre.repository';
@@ -5,14 +6,19 @@ import { IUseCase } from '@/core/shared/application/use-case.interface';
 import { NotFoundError } from '@/core/shared/domain/errors/not-found';
 
 export class GetGenreUseCase implements IUseCase<GetGenreInput, GetGenreOutput> {
-  constructor(private readonly genreRepository: IGenreRepository) {}
+  constructor(
+    private readonly genreRepository: IGenreRepository,
+    private readonly categoryRepository: ICategoryRepository,
+  ) {}
 
   async execute(input: GetGenreInput): Promise<GetGenreOutput> {
     const genre = await this.genreRepository.findById(new GenreId(input.id));
 
     if (!genre) throw new NotFoundError(input.id, Genre);
 
-    return GenreOutputMapper.toOutput(genre);
+    const categories = await this.categoryRepository.findByIds(Array.from(genre.categories_id.values()));
+
+    return GenreOutputMapper.toOutput(genre, categories);
   }
 }
 
