0a2bfee726fb1817981c38e557a75b7a171d9b4c
netoudi <netoudi@rocketmail.com>
2024-10-05 11:15:15 -0300

    dto-de-genres

--- src/modules/genres-module/dto/search-genres.dto.ts
+++ src/modules/genres-module/dto/search-genres.dto.ts
@@ -1,10 +1,3 @@
-import { ListGenresFilter, ListGenresInput } from '@/core/genre/application/use-cases/list-genre/list-genres.input';
-import { SortDirection } from '@/core/shared/domain/repository/search-params';
+import { ListGenresInput } from '@/core/genre/application/use-cases/list-genre/list-genres.input';
 
-export class SearchGenresDto implements ListGenresInput {
-  page?: number;
-  per_page?: number;
-  sort?: string;
-  sort_dir?: SortDirection;
-  filter?: ListGenresFilter;
-}
+export class SearchGenresDto extends ListGenresInput {}
