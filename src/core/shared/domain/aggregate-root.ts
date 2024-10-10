30316dd63b6f56008724679578c69d2be6500bbe
netoudi <netoudi@rocketmail.com>
2024-10-10 09:22:23 -0300

    criando-base-dos-eventos-de-dominio

--- src/core/shared/domain/aggregate-root.ts
+++ src/core/shared/domain/aggregate-root.ts
@@ -1,3 +1,12 @@
 import { Entity } from '@/core/shared/domain/entity';
+import { IDomainEvent } from '@/core/shared/domain/events/domain-event.interface';
 
-export abstract class AggregateRoot extends Entity {}
+export abstract class AggregateRoot extends Entity {
+  applyEvent(event: IDomainEvent) {
+    //
+  }
+
+  registerHandler(event: sgring, handler: (event: IDomainEvent) => void) {
+    //
+  }
+}
