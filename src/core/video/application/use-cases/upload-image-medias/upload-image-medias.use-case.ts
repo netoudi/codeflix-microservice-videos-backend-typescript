fbc18431c5d33c38c43c1c9c105eb8a970e52083
netoudi <netoudi@rocketmail.com>
2024-10-14 10:55:55 -0300

    criando-caso-de-upload-de-upload-das-imagens-parte-2

--- src/core/video/application/use-cases/upload-image-medias/upload-image-medias.use-case.ts
+++ src/core/video/application/use-cases/upload-image-medias/upload-image-medias.use-case.ts
@@ -1,9 +1,50 @@
 import { IUseCase } from '@/core/shared/application/use-case.interface';
+import { NotFoundError } from '@/core/shared/domain/errors/not-found';
+import { IUnitOfWork } from '@/core/shared/domain/repository/unit-of-work.interface';
+import { EntityValidationError } from '@/core/shared/domain/validators/validation.error';
 import { UploadImageMediasInput } from '@/core/video/application/use-cases/upload-image-medias/upload-image-medias.input';
+import { Banner } from '@/core/video/domain/banner.vo';
+import { ThumbnailHalf } from '@/core/video/domain/thumbnail-half.vo';
+import { Thumbnail } from '@/core/video/domain/thumbnail.vo';
+import { Video, VideoId } from '@/core/video/domain/video.aggregate';
+import { IVideoRepository } from '@/core/video/domain/video.repository';
 
 export class UploadImageMediasUseCase implements IUseCase<UploadImageMediasInput, UploadImageMediasOutput> {
-  execute(input: UploadImageMediasInput): Promise<UploadImageMediasOutput> {
-    throw new Error('Method not implemented.');
+  constructor(
+    private readonly uow: IUnitOfWork,
+    private readonly videoRepository: IVideoRepository,
+  ) {}
+
+  async execute(input: UploadImageMediasInput): Promise<UploadImageMediasOutput> {
+    const video = await this.videoRepository.findById(new VideoId(input.video_id));
+
+    if (!video) throw new NotFoundError(input.video_id, Video);
+
+    const imagesMap = {
+      banner: Banner,
+      Thumbnail: Thumbnail,
+      thumbnail_half: ThumbnailHalf,
+    };
+
+    const [image, errorImage] = imagesMap[input.field].createFromFile({ ...input, video_id: video.id }).asArray();
+
+    if (errorImage) {
+      throw new EntityValidationError([{ [input.field]: [errorImage.message] }]);
+    }
+
+    image instanceof Banner && video.replaceBanner(image);
+    image instanceof Thumbnail && video.replaceThumbnail(image);
+    image instanceof ThumbnailHalf && video.replaceThumbnailHalf(image);
+
+    await this.storage.store({
+      id: image.url,
+      data: input.file.data,
+      mime_type: input.file.mime_type,
+    });
+
+    await this.uow.do(async () => {
+      return this.videoRepository.update(video);
+    });
   }
 }
 
