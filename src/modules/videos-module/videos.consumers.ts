007e10bada09a5bda3e6cebf04007daa6ae1a181
netoudi <netoudi@rocketmail.com>
2024-11-15 11:26:56 -0300

    criando-consumidor-do-video

---
+++ src/modules/videos-module/videos.consumers.ts
@@ -0,0 +1,22 @@
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+import { Injectable } from '@nestjs/common';
+
+type OnProcessVideoCommand = {
+  video: {
+    resource_id: string;
+    encoded_video_folder: string;
+    status: 'COMPLETED' | 'FAILED';
+  };
+};
+
+@Injectable()
+export class VideoConsumers {
+  @RabbitSubscribe({
+    exchange: 'amq.direct',
+    routingKey: 'videos.convert',
+    queue: 'micro-videos/videos.convert',
+  })
+  onProcessVideo(msg: OnProcessVideoCommand) {
+    console.log(msg);
+  }
+}
