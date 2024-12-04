f9dca79bd16a84da9fa1f0fc83677520e88c3d73
netoudi <netoudi@rocketmail.com>
2024-12-04 20:34:00 -0300

    gerando-um-certificado-rsa-proprio

---
+++ generate-token.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { config } = require('dotenv');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const jwt = require('jsonwebtoken');
+
+//read the .env file from envs folder
+config({
+  path: __dirname + '/envs/.env',
+});
+
+//create a token
+const token = jwt.sign(
+  {
+    realm_access: {
+      roles: ['admin-catalog'],
+    },
+  },
+  process.env.JWT_PRIVATE_KEY,
+  { expiresIn: '24h', algorithm: 'RS256' },
+);
+
+console.log(token);
